57f56f26220c66233d84bb30cafad0866daddd8f
TyQuincia Oby <tyquinciaoby@gmail.com>
2025-10-30 20:39:30 -0500

    venue and airport supabase databases added

--- src/components/airports.jsx
+++ src/components/airports.jsx
@@ -5,15 +5,27 @@ import supabase from "../utils/supabase";
 
 export default function Airports(){
     const [airports, setAirports] = useState([]);
+    const airportList = [];
 
     async function getAirports() {
-        const {data , error } = await supabase("airports").select()
-        
+        const result = await supabase.from("airports").select().order("country")
+        const data = result.data;
+        setAirports(data);
+    }
+
+    for (let i = 0; i < airports.length; i++){
+        airportList.push(
+            <li key={airports[i].id}>
+                {airports[i].rank} {airports[i].year} {airports[i].airport} {airports[i].airport_code} {airports[i].location} {airports[i].country}
+            </li>
+        )
     }
 
     return(
         <>
             <h1>Busiest Airports in The USA</h1>
+            {airportList}
+            <button onClick={getAirports}>Let's Fly!</button>
         </>
     );
 }
--- src/components/venues.jsx
+++ src/components/venues.jsx
@@ -1,14 +1,41 @@
 import {useState} from "react";
 import "../App.css"
+import supabase from "../utils/supabase";
 
 export default function Venues(){
     const [venues, setVenues] = useState([]);
+
+    //retrieve venue list
+    async function handleVenues(){
+        console.log("Venue button clicked")
+        //storing result of fetch from supabase into a variable
+        const result = await supabase.from("venues").select().order("rating");
+        const data = result.data;
+        setVenues(data);
+    }
+
+
+    //empty list for venues
+    const venueList = [];
+
+   //put venue list into a list item table with attributes attached
+    for(let i = 0; i < venues.length ; i++){
+        venueList.push(
+            <li key={venues[i].id}>
+                {venues[i].thumbnail} {venues[i].title} {venues[i].address} {venues[i].rating} {venues[i].website}
+            </li>
+        )
+    }
 
 
 
     return(
         <>
             <h1> Explore Our Venue Library!</h1>
+            <ul>
+                {venueList}
+            </ul>
+            <button onClick={handleVenues}>Venue List</button>
         </>
     );
 }
